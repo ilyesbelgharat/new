c9f6a7a515558b4df804676e1cd17a6cff96160a
ilyesbelgharat <53319470+ilyesbelgharat@users.noreply.github.com>
2019-07-27 01:18:33 -0400

    Update factor.service.ts

--- src/services/factor.service.ts
+++ src/services/factor.service.ts
@@ -22,57 +22,57 @@ export class FactorService {
   }
 
   getFactor(id: number) {
-    return this.http.get('http://localhost:8080/factors/' + id,{headers:this.headersT});
+    return this.http.get('https://ilyesapprisk.herokuapp.com/factors/' + id,{headers:this.headersT});
   }
 
   getFactors() {
-    return this.http.get('http://localhost:8080/factors',{headers:this.headersT});
+    return this.http.get('https://ilyesapprisk.herokuapp.com/factors',{headers:this.headersT});
   }
 
   getFactorCode(code:String) {
-    return this.http.get('http://localhost:8080/factor/'+code,{headers:this.headersT});
+    return this.http.get('https://ilyesapprisk.herokuapp.com/factor/'+code,{headers:this.headersT});
   }
 
   getSourceCode(idFactor:number,code:String) {
-    return this.http.get('http://localhost:8080/factor/'+idFactor+"/"+code,{headers:this.headersT});
+    return this.http.get('https://ilyesapprisk.herokuapp.com/factor/'+idFactor+"/"+code,{headers:this.headersT});
   }
 
   saveFactor(factor: Factor) {
     console.log(factor);
 
 
-    return this.http.post('http://localhost:8080/factors', factor,{headers:this.headersT});
+    return this.http.post('https://ilyesapprisk.herokuapp.com/factors', factor,{headers:this.headersT});
   }
 
   deleteFactor(id: number) {
-    return this.http.delete('http://localhost:8080/factors/' + id,{headers:this.headersT});
+    return this.http.delete('https://ilyesapprisk.herokuapp.com/factors/' + id,{headers:this.headersT});
   }
 
   listSourceFactor(idFactor: number){
-    return this.http.get('http://localhost:8080/factors/'+idFactor+'/source',{headers:this.headersT});
+    return this.http.get('https://ilyesapprisk.herokuapp.com/factors/'+idFactor+'/source',{headers:this.headersT});
 
   }
 
   saveSourceFactor(idFactor:number, sourcce : SourceDanger){
     console.log(this.headersT);
-    return this.http.post('http://localhost:8080/factors/'+idFactor+'/source',sourcce,{headers:this.headersT});
+    return this.http.post('https://ilyesapprisk.herokuapp.com/factors/'+idFactor+'/source',sourcce,{headers:this.headersT});
 
   }
 
   nbreSourceId(idFactor:number){
-    return this.http.get('http://localhost:8080/factors/'+idFactor+'/nbreSource',{headers:this.headersT});
+    return this.http.get('https://ilyesapprisk.herokuapp.com/factors/'+idFactor+'/nbreSource',{headers:this.headersT});
 
   }
 
   listNbreSource(){
-    return this.http.get('http://localhost:8080/factors/nbresource',{headers:this.headersT});
+    return this.http.get('https://ilyesapprisk.herokuapp.com/factors/nbresource',{headers:this.headersT});
 
 
   }
 
 
   updateFactor(id:number, factor:Factor) {
-    return this.http.put('http://localhost:8080/factors/' + id, factor,{headers: this.headersT});
+    return this.http.put('https://ilyesapprisk.herokuapp.com/factors/' + id, factor,{headers: this.headersT});
 
   }
 
